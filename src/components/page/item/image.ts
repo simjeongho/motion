8f39feecd0b55957fef875f8a9647c85d5e0b048
simjeongho <simjeongho012@gmail.com>
2021-08-04 01:13:09 +0900

    BaseComponent 로 Component refactoring

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,24 +1,19 @@
-export class ImageComponent {
-    private element: HTMLElement;
-    constructor(title: string, urls: string) {
-        const template = document.createElement('template');
-        template.innerHTML = `<section class="image">
+import { BaseComponent } from '../../component.js';
+
+export class ImageComponent extends BaseComponent<HTMLElement>{
+    constructor(title: string, url: string) {
+        super(`<section class="image">
         <div class="image__holder">
             <img src="" alt="" class="image__thumbnails">
         </div>
         <p class="image__title"></p>
-    </section>`; //HTML요소를 입력해주고
-
-        this.element = template.content.firstElementChild! as HTMLElement;//this에 할당해주고
-
+    </section>`);
         const imageElement = this.element.querySelector('.image__thumbnails')! as HTMLImageElement;
-        imageElement.src = urls;
-        imageElement.alt = title; // 각각의 속성들을 정의해준다. 
+        imageElement.src = url;
+        imageElement.alt = title;
 
         const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
         titleElement.textContent = title;
     }
-    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-        parent.insertAdjacentElement(position, this.element)
-    }
 }
+
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,12 +1,7 @@
-export class PageComponent {
-    private element: HTMLUListElement;
-    constructor() {
-        this.element = document.createElement('ul');
-        this.element.setAttribute('class', 'page'); //html속성 정해주기
-        this.element.textContent = 'This is PageComponent';
-    }
+import { BaseComponent } from '../component.js';
 
-    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-        parent.insertAdjacentElement(position, this.element)
+export class PageComponent extends BaseComponent<HTMLUListElement> {
+    constructor() {
+        super('<ul class="page"> This is page component </ul>')
     }
 }
